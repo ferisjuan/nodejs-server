d331becb34dc57a9ab5dd1ffb2c93420d166b808
Juan Feris <feris.juan@gmail.com>
2018-12-07 16:33:33 -0500

    removed http service from app

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const http = require('http');
-
 const express = require('express');
 const app = express();
 
-const server = http.createServer(app);
-server.listen(3000);
+app.use((req, res, next) => {
+	console.log('I`m the middleware!!');
+	next();
+});
+
+app.use((req, res, next) => {
+	console.log('I`m the other middleware!!');
+	res.send('<h1>Hello!!!</h1>');
+});
+
+app.listen(3000)
 
 console.log('Listening at port 3000');
