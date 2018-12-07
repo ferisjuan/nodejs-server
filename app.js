9567f377dce475a246fddadac89dc31ff3c1e4e6
Juan Feris <feris.juan@gmail.com>
2018-12-07 17:48:16 -0500

    Added comments

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+// Node modules
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// App definition
+const app = express();
+
+// local modules
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
-const app = express();
-
+// get body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// get routes
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-
+// set listener
 app.listen(3000)
-
 console.log('Listening at port 3000');
