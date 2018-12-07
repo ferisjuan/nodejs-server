6e2c17110b8031aae3fa179f9da29584f34839ee
Juan Feris <feris.juan@gmail.com>
2018-12-07 16:47:40 -0500

    Made the assignment

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-	console.log('I`m the middleware!!');
-	next();
+app.use('/users', (req, res, next) => {
+	console.log('This is the users route');
+	res.send('<h1>Hi, it`s nice to have you here</h1>');
 });
 
-app.use((req, res, next) => {
-	console.log('I`m the other middleware!!');
-	res.send('<h1>Hello!!!</h1>');
+app.use('/', (req, res, next) => {
+	console.log('This is the home route');
+	res.send('<h1>Hello, this is the home page!!!</h1>');
 });
 
 app.listen(3000)
