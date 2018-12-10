79402beebf58ffb3fec0c9ed19480fe3c6d4524a
Juan Feris <feris.juan@gmail.com>
2018-12-10 15:05:45 -0500

    added templating engines

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // local modules
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require('./routes/admin');
+const shopData = require('./routes/shop');
 
 // get body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // get routes
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminData.routes);
+app.use(shopData.routes);
 
 app.use((req, res, next) => {
 	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+	console.log(adminData.products);
+
 	// the join method construct the files system path so it can work in linux or in windows sistems
 	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 })
 
-module.exports = router;
+exports.routes = router;
