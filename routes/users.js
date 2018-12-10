38e161064f42426b412b3af1c7be3956c3a5faf9
Juan Feris <feris.juan@gmail.com>
2018-12-10 11:40:15 -0500

    Added a static js file

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/users', (req, res, next) => {
+router.get('/', (req, res, next) => {
 	// the join method construct the files system path so it can work in linux or in windows sistems
 	res.sendFile(path.join(rootDir, 'views', 'users.html'));
 })
